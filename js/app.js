dfdf91bd1ea861f8c8c7805f13f7c7b7bc3f2da5
johnjordan1985 <johnjordan1985@gmail.com>
2019-05-30 18:15:24 -0700

    Removes un-needed wrapper function, placing encrypt cipher functions within on-click handler.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,27 @@ const numberInputs = 8;
 
 $("#encrypt_key_list").append(`<label for="user_cipher_key">Cipher Key: </label>`); // avoids HTML validation error regards 'hidden' input element, referenced by 'for' attr
 
+function decorateDocFragEncryptionKeys(docFrag, listOfCoPrimes, inputElemID = 'encrypt_key_') {
+	let inputElem, labelInputElem;
+	$.each(listOfCoPrimes, (i, number) => {
+		inputElem = $("<input id='" + inputElemID + number + "' type='radio' value='" + number + "' name='encrypt-key' />"), labelInputElem = $("<label for='" + inputElemID + number + "' >" + number + "</label>");
+		docFrag.append(inputElem.get(0),labelInputElem.get(0));
+	});
+}
+
+function getUserEncryptionKey(){
+	return $("#encrypt_key_list input[name='encrypt-key']:checked").val();
+}
+
+
+function addEncryptionKeysDOM(listOfCoPrimes) {
+	let docFrag = document.createDocumentFragment();
+	decorateDocFragEncryptionKeys(docFrag, listOfCoPrimes);
+	docFrag.firstChild.checked = true;
+	$("#encrypt_key_list").append(docFrag);
+}
+
+
 function addUserInputOutputToDOM(numElementsToAdd) {
 	let $docFragInputs = $(document.createDocumentFragment());
 	let $docFragOutputs = $(document.createDocumentFragment());
@@ -26,14 +47,12 @@ function getNewListOfUserInputs() {
 }
 
 
-
 function updateRelevantOutputElement(outputIndex, encryptedValue){
 	document.getElementById("output_char_" + outputIndex).innerHTML = encryptedValue; // rather than value property, as want to include HTML entities
 }
 
-
-function runCipher(userEncryptionKey) {
-	let $listOfUserInputs = $(getNewListOfUserInputs());
+// Iterates through output elements, and sets their value with match input element's encrytped value
+function setOutputElements($listOfUserInputs, userEncryptionKey) {
 	let encryptedCharValue;
 	$listOfUserInputs.each((i,e) => {
 		encryptedCharValue = getEncryptedCharacterValue($listOfUserInputs[i], userEncryptionKey); // Need to pass in user selected key
@@ -41,38 +60,13 @@ function runCipher(userEncryptionKey) {
 	});
 }
 
-
-function decorateDocFragEncryptionKeys(docFrag, listOfCoPrimes, inputElemID = 'encrypt_key_') {
-	let inputElem, labelInputElem;
-	$.each(listOfCoPrimes, (i, number) => {
-		inputElem = $("<input id='" + inputElemID + number + "' type='radio' value='" + number + "' name='encrypt-key' />"), labelInputElem = $("<label for='" + inputElemID + number + "' >" + number + "</label>");
-		docFrag.append(inputElem.get(0),labelInputElem.get(0));
-	});
-}
-
-
-function addEncryptionKeysDOM(listOfCoPrimes) {
-	let docFrag = document.createDocumentFragment();
-	decorateDocFragEncryptionKeys(docFrag, listOfCoPrimes);
-	docFrag.firstChild.checked = true;
-	$("#encrypt_key_list").append(docFrag);
-}
-
-function getUserEncryptionKey(){
-	return $("#encrypt_key_list input[name='encrypt-key']:checked").val();
-}
-
-
 addUserInputOutputToDOM(numberInputs);
 addEncryptionKeysDOM(getCoPrimes(27));
-
-
-
 let $userInputChar1 = $("#user_char_1");
 let $outputChar1 = $("#output_char_1");
 
 $('#encrypt_button').click(() => {
 	let userEncryptionKey = getUserEncryptionKey();
-	runCipher(userEncryptionKey);
-
+	let $listOfUserInputs = $(getNewListOfUserInputs());
+	setOutputElements($listOfUserInputs, userEncryptionKey);
 });
