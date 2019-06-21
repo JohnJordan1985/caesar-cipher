c50aa80a3ee60ec0e690f583113ddeb87f7dc4b0
John Jordan <johnjordan1985@gmail.com>
2019-06-21 13:13:52 -0700

    replaces magic modulus value with constant, length of character array.

--- js/encrypt.js
+++ js/encrypt.js
@@ -1,5 +1,6 @@
 //let alphabetLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const alphabetUpperCase = ["--", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const modulus = alphabetUpperCase.length;
 
 function getNumericalValueOfLetter(letter) {
 	var letterToCheck = letter.toString().toUpperCase();
@@ -11,6 +12,6 @@ function getNumericalValueOfLetter(letter) {
 export default function getEncryptedCharacterValue(plainTextChar, userEncryptionKey) {
 	let numericalValuePlainTextChar = getNumericalValueOfLetter(plainTextChar);
 	numericalValuePlainTextChar = (numericalValuePlainTextChar >= 0) ? numericalValuePlainTextChar : 0 ; // catch cases where '-1' if not in alphabetUpperCase array
-	let encryptedIndexValueNotOffset = (numericalValuePlainTextChar*userEncryptionKey)%27;
+	let encryptedIndexValueNotOffset = (numericalValuePlainTextChar*userEncryptionKey)%modulus;
 	return alphabetUpperCase[encryptedIndexValueNotOffset];
 }
